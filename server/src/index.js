b46b03aba42112c881305f01ae8567d5d30c4f3e
Utsav Joshi <joshiutsav56@gmail.com>
2024-01-18 18:26:49 +0530

    created a server successfully

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const helmet = require("helmet");
+import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json()); // Parse incoming JSON requests
 app.use(helmet()); // Use Helmet for basic security headers
 
 // Sample route
-app.get("/", (req, res) => {
-  res.send("Hello, this is your Express server!");
+app.get('/', (req, res) => {
+  res.send('Welcome to your Express server!');
 });
 
 // Error handling middleware
-app.use((error, req, res, next) => {
-  console.error(error);
-  res.status(500).json({ error: "Internal Server Error" });
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({error: 'Internal Server Error'});
 });
 
 // Start the server

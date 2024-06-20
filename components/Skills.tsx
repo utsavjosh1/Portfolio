004c427dce58d1e8f4534e0c4bff1e966be4cc97
Utsav Joshi <joshiutsav56@gmail.com>
2024-06-21 01:45:34 +0530

    🐛 fix: change some aboutme issue

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,7 +5,7 @@ interface SkillsUsedProps {
 
 const SkillsUsedInProjects: React.FC<SkillsUsedProps> = ({ name, img }) => {
   return (
-    <div className="text-black-300 cursor-pointer bg-slate-100 opacity-80 rounded-lg border-[0.5px] border-opacity-40 w-[130px] flex justify-around gap-2 items-center m-3 flex-wrap hover:bg-slate-300 p-2 transition-all duration-200 ">
+    <div className="text-black-300 bg-slate-100 opacity-80 rounded-lg w-[130px] flex justify-around items-center m-3 p-2">
       <div className="rounded-md ">
         <img src={img} width={20} alt={name} />
       </div>

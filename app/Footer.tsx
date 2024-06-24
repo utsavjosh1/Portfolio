9f64ce1a2100900da50e0e37eadf5e92de1298ec
Utsav Joshi <joshiutsav56@gmail.com>
2024-06-25 02:12:19 +0530

    🚑 fix: fixed the whole layout of the page and overwritten code

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className="text-slate-100 bg-slate-800 p-5 my-5 mb-7 opacity-80 text-mg md:w-[88%] rounded-lg text-center items-center justify-center">
-      Utsav Joshi © {currentYear}  All Rights Reserved -- utsavjoshi602@gmail.com
+      Utsav Joshi © {currentYear} All Rights Reserved utsavjoshi602@gmail.com
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -9,117 +9,85 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="w-screen h-screen ">
-      <div className="overlay"></div>
+    <div className="relative h-screen w-screen">
       <video
         src="/video/bg.mp4"
         loop
         autoPlay
         muted
         playsInline
-        className="h-[100%] w-[100%] object-cover "
+        className="absolute top-0 left-0 w-full h-full object-cover"
       ></video>
-      <div className="content flex">
-        <div className="w-[640px]  m-auto border-[0.05px] border-opacity-5 border-white  h-[auto] ">
-          <div className="px-4 pt-2 absolute text-white md:hidden">
-            <Sheet>
-              <SheetTrigger>
-                <img src="/burger.png" alt="" />{" "}
-              </SheetTrigger>
-              <SheetContent className="bg-slate-950 bg-opacity-40 border-none text-slate-400">
-                <SheetHeader>
-                  <SheetTitle className="text-slate-400 text-center text-xl">
-                    CONNECT WITH ME
-                  </SheetTitle>
-                  <SheetDescription className="text-slate-400">
-                    <div className="text-slate-400 flex flex-col  w-[50px] m-auto h-70% justify-evenly   gap-6 ">
-                      <div>
-                        <Link
-                          target="blank"
-                          href={"https://twitter.com/joshi__utsav"}
-                        >
-                          <img
-                            src="/twitterWhite.png"
-                            className="cursor-pointer mt-8"
-                            width={37}
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div>
-                        <Link
-                          target="blank"
-                          href={"https://github.com/joshiUtsav"}
-                        >
-                          <img
-                            src="/githubwhite.png"
-                            className="cursor-pointer"
-                            width={37}
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div>
-                        <Link
-                          target="blank"
-                          href={"https://www.linkedin.com/in/joshi-utsav/"}
-                        >
-                          <img
-                            src="/linkedinwhite.png"
-                            className="cursor-pointer"
-                            width={37}
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div className="h-[100px] border border-white w-[1px] ml-5 "></div>
-                    </div>
-                  </SheetDescription>
-                </SheetHeader>
-              </SheetContent>
-            </Sheet>
-          </div>
-          <Content />
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-65"></div>
+      <DrawerComponent />
+      <div className="relative flex flex-row-reverse text-center items-center justify-center z-10">
+        <div className="flex-1 flex justify-center items-center">
+          <IconLinkLogo />
         </div>
-        <div className=" flex-col hidden md:flex h-screen justify-center m-5 gap-6  mt-28">
-          <div>
-            <Link target="blank" href={"https://twitter.com/joshi__utsav"}>
-              <img
-                src="/twitterWhite.png"
-                className="cursor-pointer"
-                width={37}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div>
-            <Link target="blank" href={"https://github.com/joshiUtsav"}>
-              <img
-                src="/githubwhite.png"
-                className="cursor-pointer"
-                width={37}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div>
-            <Link
-              target="blank"
-              href={"https://www.linkedin.com/in/joshi-utsav/"}
-            >
-              <img
-                src="/linkedinwhite.png"
-                className="cursor-pointer"
-                width={37}
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className="h-[100px] border border-white w-[1px] ml-5 "></div>
+        <div className="flex-1 flex justify-center items-center">
+          <Content />
         </div>
       </div>
     </div>
   );
-}
+};
+
+const DrawerComponent = () => {
+  return (
+    <div className="md:hidden h-full">
+      <div className="m-5 absolute text-white">
+        <Sheet>
+          <SheetTrigger>
+            <img src="/burger.png" alt="MenuButton" />
+          </SheetTrigger>
+          <SheetContent className="bg-slate-950 bg-opacity-40 border-none text-slate-400">
+            <SheetHeader>
+              <SheetTitle className="text-slate-400 text-center mt-10 text-3xl">
+                CONNECT WITH ME
+              </SheetTitle>
+              <SheetDescription>
+                <IconLinkLogo />
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  );
+};
+
+const IconLinkLogo = () => {
+  return (
+    <div className="flex flex-col items-center text-slate-200 gap-6 mt-5">
+      <Link target="_blank" href={"https://twitter.com/joshi__utsav"}>
+        <img
+          src="/twitterWhite.png"
+          className="cursor-pointer"
+          width={37}
+          alt="twitter"
+        />
+      </Link>
+      <Link target="_blank" href={"https://github.com/joshiUtsav"}>
+        <img
+          src="/githubwhite.png"
+          className="cursor-pointer"
+          width={37}
+          alt="github"
+        />
+      </Link>
+      <Link target="_blank" href={"https://www.linkedin.com/in/joshi-utsav/"}>
+        <img
+          src="/linkedinwhite.png"
+          className="cursor-pointer"
+          width={37}
+          alt="linkedIn"
+        />
+      </Link>
+      <div className="h-[100px] bg-white w-[1px] hidden sm:block"></div>
+    </div>
+  );
+};
+
+export default Home;

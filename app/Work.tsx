e36dedca99697d4bcaa0c29b80537ee267de1d06
Utsav Joshi <joshiutsav56@gmail.com>
2024-07-07 16:02:32 +0530

    fix: fixed the project scroll

--- app/Work.tsx
+++ app/Work.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import ProjectCard from "@/components/ProjectCard";
 import TitleTag from "@/components/Title";
 import { projects } from "@/lib/data";
-
 
 interface Project {
   p_name: string;
@@ -16,17 +17,51 @@ interface Project {
  * @returns {React.ReactElement} The rendered "Project" section.
  */
 const Project = (): React.ReactElement => {
+  const projectContainerRef = useRef<HTMLDivElement>(null);
+
+  const scrollRight = () => {
+    if (projectContainerRef.current) {
+      projectContainerRef.current.scrollBy({
+        left: 300,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollLeft = () => {
+    if (projectContainerRef.current) {
+      projectContainerRef.current.scrollBy({
+        left: -300,
+        behavior: "smooth",
+      });
+    }
+  };
+
   const projectElements = projects.map((project: Project, index: number) => (
-    <ProjectCard
-      key={index}
-      {...project}
-    />
+    <ProjectCard key={index} {...project} />
   ));
 
   return (
     <div className="mt-[5rem] p-4 md:w-[100%] md:h-full">
-      <TitleTag tagName={"Projects"} />
-      <div className="flex md:flex-row overflow-x-auto no-scrollbar">
+      <div className="flex justify-between items-center">
+        <TitleTag tagName={"Projects"} />
+        <button
+          className="ml-4 p-2 bg-slate-800 text-white rounded-md"
+          onClick={scrollLeft}
+        >
+          {"<"}
+        </button>
+        <button
+          className="ml-4 p-2 bg-slate-800 text-white rounded-md"
+          onClick={scrollRight}
+        >
+          {">"}
+        </button>
+      </div>
+      <div
+        className="flex md:flex-row overflow-x-auto no-scrollbar"
+        ref={projectContainerRef}
+      >
         {projectElements}
       </div>
     </div>

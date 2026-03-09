3888b5284ed02b2f6a87dea41ecc5af5abfbc559
Utsav joshi <utsav.joshi@nextbill.io>
2026-03-09 23:06:00 +0530

    chore: upgrade dependencies

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,15 @@
 export interface ContactSubmission {
-  id: string
-  name: string
-  email: string
-  message: string
-  createdAt: Date
+  id: string;
+  name: string;
+  email: string;
+  message: string;
+  createdAt: Date;
 }
 
 export interface PageView {
-  id: string
-  path: string
-  ipAddress?: string | null
-  userAgent?: string | null
-  createdAt: Date
+  id: string;
+  path: string;
+  ipAddress?: string | null;
+  userAgent?: string | null;
+  createdAt: Date;
 }
